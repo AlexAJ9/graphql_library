227365d706c4e4c648a8a8dd28235e681da2a17e
theoc <a4319@mail.bg>
2020-02-20 16:05:37 +0200

    books view

--- library-frontend/src/components/Books.js
+++ library-frontend/src/components/Books.js
@@ -1,38 +1,55 @@
 import React from 'react'
+import { gql } from 'apollo-boost'
+import { Query } from 'react-apollo'
+
+const ALL_BOOKS = gql`
+  {
+    allBooks{ 
+      title
+      published
+      author
+    }
+  }
+`
 
 const Books = (props) => {
   if (!props.show) {
     return null
   }
 
+
   const books = []
+  return <Query query={ALL_BOOKS}>
+    {(result) => {
+      if (result.loading) { return <div>loading...</div> }
+      return (
+        <div>
+          <h2>books</h2>
 
-  return (
-    <div>
-      <h2>books</h2>
-
-      <table>
-        <tbody>
-          <tr>
-            <th></th>
-            <th>
-              author
+          <table>
+            <tbody>
+              <tr>
+                <th></th>
+                <th>
+                  author
             </th>
-            <th>
-              published
+                <th>
+                  published
             </th>
-          </tr>
-          {books.map(a =>
-            <tr key={a.title}>
-              <td>{a.title}</td>
-              <td>{a.author}</td>
-              <td>{a.published}</td>
-            </tr>
-          )}
-        </tbody>
-      </table>
-    </div>
-  )
+              </tr>
+              {result.data.allBooks.map(a =>
+                <tr key={a.title}>
+                  <td>{a.title}</td>
+                  <td>{a.author}</td>
+                  <td>{a.published}</td>
+                </tr>
+              )}
+            </tbody>
+          </table>
+        </div>
+      )
+    }}
+  </Query>
 }
 
 export default Books
